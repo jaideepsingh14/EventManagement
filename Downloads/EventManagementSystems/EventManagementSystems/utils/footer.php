f073512981bcbbb7f7354e07f70ff29c5898ccae
Jaideep Singh <jaideep.solanki99@gmail.com>
2024-01-23 23:17:17 +0530

    updating project

---
+++ Downloads/EventManagementSystems/EventManagementSystems/utils/footer.php
@@ -0,0 +1,33 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>W</strong><span class = "small footerSubtext">eb</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                        <strong>M</strong><span class = "small footerSubtext">ini</span>
+                        <strong>P</strong><span class = "small footerSubtext">roject</span>
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Done By:<br>
+                    Jaideep Singh Solanki (1BG17CS033)<br>
+                    Jayanth Bhansali (1BG17CS036)
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
